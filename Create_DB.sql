c990d6f9b72506baef88a88e67d6612659d1eeac
RahulJ-Sepia <141811708+RahulJ-Sepia@users.noreply.github.com>
2023-08-17 17:46:16 +0530

    Add files via upload

---
+++ Create_DB.sql
@@ -0,0 +1,13 @@
+-- Database: Forecast_Engine
+
+-- DROP DATABASE IF EXISTS "Forecast_Engine";
+
+CREATE DATABASE "Forecast_Engine"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
